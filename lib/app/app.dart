03636df78c9ac7c6b3d723ac07996dc32f24901e
Wojtek <wojtek.koziol.pl@gmail.com>
2022-05-13 16:14:57 +0200

    Cleanup files

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,10 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+import 'package:flutter/material.dart';
 
-export 'view/app.dart';
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
