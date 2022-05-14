3e1682078528c3401e2816c96bd9b52b3bfe8788
Wojtek <wojtek.koziol.pl@gmail.com>
2022-05-14 20:44:24 +0200

    Add CalcCubit

---
+++ lib/calc/calc_cubit.dart
@@ -0,0 +1,77 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'calc_state.dart';
+part 'calc_cubit.freezed.dart';
+
+class CalcCubit extends Cubit<CalcState> {
+  CalcCubit() : super(const CalcState.result());
+
+  final _chars = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+  String _getTyped(String n, String char) {
+    if (char == '.' && n.contains('.')) return n;
+    if (n == '0' && char != '.') return char;
+    return '$n$char';
+  }
+
+  void type(String char) {
+    if (!_chars.contains(char)) return;
+    final newState = state.map(
+      result: (value) => value.copyWith(result: _getTyped(value.result, char)),
+      add: (value) => value.copyWith(b: _getTyped(value.b ?? '0', char)),
+      subtract: (value) => value.copyWith(b: _getTyped(value.b ?? '0', char)),
+      multiply: (value) => value.copyWith(b: _getTyped(value.b ?? '0', char)),
+      divide: (value) => value.copyWith(b: _getTyped(value.b ?? '0', char)),
+      error: (value) => CalcState.result(_getTyped('0', char)),
+    );
+    emit(newState);
+  }
+
+  void add() {
+    result();
+    state.whenOrNull(
+      result: (result) => emit(CalcState.add(a: result)),
+    );
+  }
+
+  void subtract() {
+    result();
+    state.whenOrNull(
+      result: (result) => emit(CalcState.subtract(a: result)),
+    );
+  }
+
+  void multiply() {
+    result();
+    state.whenOrNull(
+      result: (result) => emit(CalcState.multiply(a: result)),
+    );
+  }
+
+  void divide() {
+    result();
+    state.whenOrNull(
+      result: (result) => emit(CalcState.divide(a: result)),
+    );
+  }
+
+  void result() {
+    final result = state.whenOrNull<double?>(
+      result: double.parse,
+      add: (a, b) => double.parse(a) + double.parse(b ?? '0'),
+      subtract: (a, b) => double.parse(a) - double.parse(b ?? '0'),
+      multiply: (a, b) => double.parse(a) * double.parse(b ?? '1'),
+      divide: (a, b) {
+        final n = double.parse(b ?? '1');
+        if (n == 0) return null;
+        return double.parse(a) / n;
+      },
+    );
+    if (result == null) {
+      emit(const CalcState.error('Not a number'));
+      return;
+    }
+    emit(CalcState.result('$result'));
+  }
+}
--- lib/calc/calc_state.dart
+++ lib/calc/calc_state.dart
@@ -5,19 +5,19 @@ class CalcState with _$CalcState {
   const factory CalcState.result([@Default('0') String result]) = _Result;
   const factory CalcState.add({
     required String a,
-    @Default('0') String b,
+    String? b,
   }) = _Add;
   const factory CalcState.subtract({
     required String a,
-    @Default('0') String b,
+    String? b,
   }) = _Subtract;
   const factory CalcState.multiply({
     required String a,
-    @Default('0') String b,
+    String? b,
   }) = _Multiply;
   const factory CalcState.divide({
     required String a,
-    @Default('0') String b,
+    String? b,
   }) = _Divide;
   const factory CalcState.error(String message) = _Error;
 }
