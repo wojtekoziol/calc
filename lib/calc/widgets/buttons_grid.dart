66c699bb4ae1bf151a886c19f64b55979565cf2c
Wojtek <wojtek.koziol.pl@gmail.com>
2022-07-01 12:07:21 +0200

    Implement adaptive layout

--- lib/calc/widgets/buttons_grid.dart
+++ lib/calc/widgets/buttons_grid.dart
@@ -18,120 +18,134 @@ class ButtonsGrid extends StatelessWidget {
           top: Radius.circular(kPaddingL),
         ),
       ),
-      child: GridView.count(
-        physics: const NeverScrollableScrollPhysics(),
-        crossAxisCount: 4,
-        mainAxisSpacing: kPaddingM,
-        crossAxisSpacing: kPaddingM,
-        padding: const EdgeInsets.all(kPaddingL),
+      padding: const EdgeInsets.symmetric(
+        vertical: kPaddingL,
+        horizontal: kPaddingM,
+      ),
+      child: Row(
         children: [
-          CalcButton(
-            onTap: calc.reset,
-            text: 'AC',
-            color: kGreen,
+          Expanded(
+            child: Column(
+              children: [
+                CalcButton(
+                  onTap: calc.reset,
+                  text: 'AC',
+                  color: kGreen,
+                ),
+                CalcButton(
+                  onTap: () => calc.type('7'),
+                  text: '7',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('4'),
+                  text: '4',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('1'),
+                  text: '1',
+                ),
+                CalcButton(
+                  onTap: calc.revert,
+                  text: '←',
+                ),
+              ],
+            ),
           ),
-          CalcButton(
-            onTap: calc.changeSign,
-            text: '+/-',
-            color: kGreen,
+          Expanded(
+            child: Column(
+              children: [
+                CalcButton(
+                  onTap: calc.changeSign,
+                  text: '+/-',
+                  color: kGreen,
+                ),
+                CalcButton(
+                  onTap: () => calc.type('8'),
+                  text: '8',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('5'),
+                  text: '5',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('2'),
+                  text: '2',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('0'),
+                  text: '0',
+                ),
+              ],
+            ),
           ),
-          CalcButton(
-            onTap: calc.percent,
-            text: '%',
-            color: kGreen,
+          Expanded(
+            child: Column(
+              children: [
+                CalcButton(
+                  onTap: calc.percent,
+                  text: '%',
+                  color: kGreen,
+                ),
+                CalcButton(
+                  onTap: () => calc.type('9'),
+                  text: '9',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('6'),
+                  text: '6',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('3'),
+                  text: '3',
+                ),
+                CalcButton(
+                  onTap: () => calc.type('.'),
+                  text: '.',
+                ),
+              ],
+            ),
           ),
-          BlocBuilder<CalcCubit, CalcState>(
-            builder: (_, state) {
-              return CalcButton(
-                onTap: calc.divide,
-                text: '÷',
-                color: kRed,
-                clicked: state.whenOrNull(divide: (a, b) => true) ?? false,
-              );
-            },
-          ),
-          CalcButton(
-            onTap: () => calc.type('7'),
-            text: '7',
-          ),
-          CalcButton(
-            onTap: () => calc.type('8'),
-            text: '8',
-          ),
-          CalcButton(
-            onTap: () => calc.type('9'),
-            text: '9',
-          ),
-          BlocBuilder<CalcCubit, CalcState>(
-            builder: (_, state) {
-              return CalcButton(
-                onTap: calc.multiply,
-                text: 'X',
-                color: kRed,
-                clicked: state.whenOrNull(multiply: (a, b) => true) ?? false,
-              );
-            },
-          ),
-          CalcButton(
-            onTap: () => calc.type('4'),
-            text: '4',
-          ),
-          CalcButton(
-            onTap: () => calc.type('5'),
-            text: '5',
-          ),
-          CalcButton(
-            onTap: () => calc.type('6'),
-            text: '6',
-          ),
-          BlocBuilder<CalcCubit, CalcState>(
-            builder: (_, state) {
-              return CalcButton(
-                onTap: calc.subtract,
-                text: '-',
-                color: kRed,
-                clicked: state.whenOrNull(subtract: (a, b) => true) ?? false,
-              );
-            },
-          ),
-          CalcButton(
-            onTap: () => calc.type('1'),
-            text: '1',
-          ),
-          CalcButton(
-            onTap: () => calc.type('2'),
-            text: '2',
-          ),
-          CalcButton(
-            onTap: () => calc.type('3'),
-            text: '3',
-          ),
-          BlocBuilder<CalcCubit, CalcState>(
-            builder: (_, state) {
-              return CalcButton(
-                onTap: calc.add,
-                text: '+',
-                color: kRed,
-                clicked: state.whenOrNull(add: (a, b) => true) ?? false,
-              );
-            },
-          ),
-          CalcButton(
-            onTap: calc.revert,
-            text: '←',
-          ),
-          CalcButton(
-            onTap: () => calc.type('0'),
-            text: '0',
-          ),
-          CalcButton(
-            onTap: () => calc.type('.'),
-            text: '.',
-          ),
-          CalcButton(
-            onTap: calc.result,
-            text: '=',
-            color: kRed,
+          Expanded(
+            child: BlocBuilder<CalcCubit, CalcState>(
+              builder: (context, state) {
+                return Column(
+                  children: [
+                    CalcButton(
+                      onTap: calc.divide,
+                      text: '÷',
+                      color: kRed,
+                      clicked:
+                          state.whenOrNull(divide: (a, b) => true) ?? false,
+                    ),
+                    CalcButton(
+                      onTap: calc.multiply,
+                      text: 'X',
+                      color: kRed,
+                      clicked:
+                          state.whenOrNull(multiply: (a, b) => true) ?? false,
+                    ),
+                    CalcButton(
+                      onTap: calc.subtract,
+                      text: '-',
+                      color: kRed,
+                      clicked:
+                          state.whenOrNull(subtract: (a, b) => true) ?? false,
+                    ),
+                    CalcButton(
+                      onTap: calc.add,
+                      text: '+',
+                      color: kRed,
+                      clicked: state.whenOrNull(add: (a, b) => true) ?? false,
+                    ),
+                    CalcButton(
+                      onTap: calc.result,
+                      text: '=',
+                      color: kRed,
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
         ],
       ),
